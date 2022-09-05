2d9ce53418bc4fa05d0d17822f8a52fc79158bc9
JIWON PARK <55792986+pjw960316@users.noreply.github.com>
2022-09-05 20:07:13 +0900

    [지원] #two pointer

--- CodingTest/BOJ/boj_2003.cpp
+++ CodingTest/BOJ/boj_2003.cpp
@@ -21,15 +21,14 @@
 using ll = long long;
 using namespace std;
 
-ll arr[100000];
-ll sum[100000];
+ll arr[100002];
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int p1=1, p2=1;
+    int p1=0, p2=0;
 
     ll n,m;
     cin >> n >> m;
@@ -38,52 +37,32 @@ int main()
         cin >> arr[i];
     }
 
-    sum[0] = 0;
-    for(int i=1; i<=n; i++)
-    {
-        sum[i] = sum[i-1] + arr[i];
-    }
-
     int ans = 0;
-    while(1)
-    {        
-        ll tmp = sum[p2] - sum[p1-1];
-        if(tmp == m)
+    ll sum = 0;
+    while(p1 <= p2)
+    {          
+        if(sum == m)
         {
             ans++;
             p1++;
-            p2 = p1;
-            if(p1 == n)
+            sum -= arr[p1];
+        }
+        else if(sum < m)
+        {
+            p2++;
+            sum += arr[p2];
+            if(p2 > n)
             {
                 cout << ans;
                 return 0;
-            }
-        }
-        else if(tmp < m)
-        {
-            p2++;
-            if(p2 == n+1)
-            {
-                if(p1 == n)
-                {
-                    cout << ans;
-                    return 0;
-                }
-                p1++;
-                p2 = p1;
             }
         }
         else
         {
             p1++;
-            p2 = p1;
-            if(p1 == n+1)
-            {
-                cout << ans;
-                return 0;
-            }
+            sum -= arr[p1];
         }
     }
-
-   return 0;
+    cout << ans;
+    return 0;
 }
