3c3e43b12c9d37d87c856f49c03acc055d05b8f3
JIWON PARK <55792986+pjw960316@users.noreply.github.com>
2022-10-13 22:10:50 +0900

    [지원] #pow

---
+++ CodingTest/Practice/pow.cpp
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <utility>
+#include <cmath>
+#include <ctime>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <vector>
+#include <list>
+#include <deque>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+
+using ll = long long;
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+
+    double d;
+    d = pow(1-6,2) + pow(-3-9,2);
+    cout << d <<"\n"; //169
+    cout << sqrt(d) << "\n"; //13
+
+	return 0;
+}
